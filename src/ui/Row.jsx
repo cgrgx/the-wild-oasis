b1514b5e68b83bd8b5e71398dfbf01cb40a4adbd
chiran gurung <grgchiran08@yahoo.com>
2023-12-20 23:18:16 -0500

    initial commit

---
+++ src/ui/Row.jsx
@@ -0,0 +1,25 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+    `}
+  ${(props) =>
+    props.type === "vertical" &&
+    css`
+      flex-direction: column;
+      gap: 1.6rem;
+    `}
+`;
+
+//React will automatically make a default prop to vertical, so we can ommit it while using this component
+Row.defaultProps = {
+  type: "vertical",
+};
+
+export default Row;
