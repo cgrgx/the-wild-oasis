242f5fc423df7f805cbe20c91d3faf0ee6edfedb
chiran gurung <grgchiran08@yahoo.com>
2023-12-27 12:47:15 -0500

    filter bookings by status and only fetch data from server based on filter status

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
+
 import { getBookings } from "../../services/apiBookings";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filters =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  // to filter by multiple fields
+  // :[
+  //   { field: "totalPrice", value: 5000, method: "gte" },
+  //   { field: "status", value: filterValue },
+  // ];
+
+  // SORT
+  const sortBy = searchParams.get("sortBy");
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", { filters, sortBy }],
+    queryFn: () => getBookings({ filters, sortBy }),
   });
 
   return { isLoading, bookings, error };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,22 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filters, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at,startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)"
     );
+
+  // 1. FILTER
+  if (filters !== null)
+    // To filter by multiple fields
+    // filters.forEach((filter) => {
+    //   query = query[filter.method || "eq"](filter.field, filter.value);
+    // });
+    query = query[filters.method || "eq"](filters.field, filters.value);
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
