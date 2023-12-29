59676c0ac01fee1fe99c611a62f848c1392f0bbc
chiran gurung <grgchiran08@yahoo.com>
2023-12-29 18:11:57 -0500

    implement user sign up, authorization and header

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,8 +1,15 @@
 import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import styled from "styled-components";
 
 import ButtonIcon from "../../ui/ButtonIcon";
 import { useLogout } from "./useLogout";
 import SpinnerMini from "../../ui/SpinnerMini";
+
+const Span = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+`;
 
 function Logout() {
   const { logout, isLoading } = useLogout();
