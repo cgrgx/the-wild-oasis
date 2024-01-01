1e841093fcd430ab7d7e73bf0edcd692b50d8aad
chiran gurung <grgchiran08@yahoo.com>
2023-12-31 20:37:48 -0500

    small change

--- src/features/authentication/GoogleFormNew.jsx
+++ src/features/authentication/GoogleFormNew.jsx
@@ -1,41 +1,41 @@
 import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
-import axios from "axios";
 import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 import { useState } from "react";
 import styled from "styled-components";
 
-let Button = styled.button`
-  background-color: #4285f4;
-  color: white;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-  margin-top: 10px;
-`;
+// let Button = styled.button`
+//   background-color: #d8286b;
+//   color: white;
+//   border: none;
+//   padding: 10px 20px;
+//   border-radius: 5px;
+//   cursor: pointer;
+//   font-size: 16px;
+//   margin-top: 10px;
+// `;
 
 function GoogleFormNew() {
-  const [user, setUser] = useState({});
-  const login = useGoogleLogin({
-    onSuccess: async (response) => {
-      try {
-        const res = await axios.get(
-          "https://www.googleapis.com/oauth2/v3/userinfo",
-          {
-            headers: {
-              Authorization: `Bearer ${response.access_token}`,
-            },
-          }
-        );
-        console.log(res);
-        setUser(res.data);
-      } catch (err) {
-        console.log(err);
-      }
-    },
-  });
-  console.log(user);
+  // const [user, setUser] = useState({});
+  // const login = useGoogleLogin({
+  //   onSuccess: async (response) => {
+  //     try {
+  //       const res = await axios.get(
+  //         "https://www.googleapis.com/oauth2/v3/userinfo",
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${response.access_token}`,
+  //           },
+  //         }
+  //       );
+  //       console.log(res);
+  //       setUser(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   },
+  // });
+  // console.log(user);
   return (
     <div>
       <GoogleLogin
@@ -47,7 +47,7 @@ function GoogleFormNew() {
           console.log("Login Failed");
         }}
       />
-      <Button onClick={() => login()}>Sign in with Google </Button>
+      {/* <Button onClick={() => login()}>Sign in with Google </Button> */}
     </div>
   );
 }
